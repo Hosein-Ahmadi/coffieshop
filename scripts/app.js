0d643cc0f0f4fbb451348bd2c26a60f010e593bb
Hosein Ahmadi <168641202+Hosein-Ahmadi@users.noreply.github.com>
2024-07-31 10:12:00 +0330

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -10,48 +10,73 @@ const cover = $.querySelector(".cover")
 const shopIcon = $.querySelector(".shop_icon")
 const shopList = $.querySelector(".shop_nav")
 const shopClose = $.querySelector(".shop_close")
-toggleThemeBtn.addEventListener("click" , () => {
-    if (localStorage.theme === "dark"){
+toggleThemeBtn.addEventListener("click", () => {
+    if (localStorage.theme === "dark") {
         document.documentElement.classList.remove("dark");
         localStorage.theme = "light";
     } else {
         document.documentElement.classList.add("dark");
-        localStorage.setItem("theme" , "dark");
+        localStorage.setItem("theme", "dark");
     }
 })
-toggleThemeBtn2.addEventListener("click" , () => {
-    if (localStorage.theme === "dark"){
+toggleThemeBtn2.addEventListener("click", () => {
+    if (localStorage.theme === "dark") {
         document.documentElement.classList.remove("dark");
         localStorage.theme = "light";
     } else {
         document.documentElement.classList.add("dark");
-        localStorage.setItem("theme" , "dark");
+        localStorage.setItem("theme", "dark");
     }
 })
-chevron.addEventListener("click",(e) =>{
+chevron.addEventListener("click", (e) => {
     console.log(e.target.parentElement);
     submenu.classList.toggle("submenu_open")
 })
-bars.addEventListener("click" , () => {
-    nav.classList.add("mobile_nav_open")
-    cover.classList.add("cover_open")
+bars.addEventListener("click", () => {
+    nav.classList.toggle("mobile_nav_open")
+    cover.classList.toggle("cover_open")
 })
 
-closeBtn.addEventListener("click" , () => {
-    nav.classList.remove("mobile_nav_open")
-    cover.classList.remove("cover_open")
+closeBtn.addEventListener("click", (e) => {
+    e.target.parentElement.parentElement.parentElement.classList.includes("mobile_nav_open")
+    nav.classList.toggle("mobile_nav_open")
+    cover.classList.toggle("cover_open")
 })
 
-cover.addEventListener("click" , ()=>{
+cover.addEventListener("click", () => {
     nav.classList.remove("mobile_nav_open")
     shopList.classList.remove("shop_nav_open")
     cover.classList.remove("cover_open")
 })
-shopIcon.addEventListener("click" , ()=>{
-    shopList.classList.add("shop_nav_open")
-    cover.classList.add("cover_open")
+shopIcon.addEventListener("click", () => {
+    shopList.classList.toggle("shop_nav_open")
+    cover.classList.toggle("cover_open")
 })
-shopClose.addEventListener("click",()=>{
-    shopList.classList.remove("shop_nav_open")
+shopClose.addEventListener("click", (e) => {
+    e.target.parentElement.parentElement.parentElement.classList.includes("mobile_nav_open")
+    shopList.classList.toggle("shop_nav_open")
     cover.classList.remove("cover_open")
 })
+
+
+const sr = ScrollReveal({
+    origin: "bottom",
+    distance: "60px",
+    duration: 1000,
+    delay: 600
+})
+
+sr.reveal(".home_allTexts", { origin: "top" })
+sr.reveal(".sec_R_title", { origin: "right",duration:500 })
+sr.reveal(".left_sec_link", { origin: "left",delay:700})
+sr.reveal(".product_Box")
+sr.reveal(".rightCategory", { origin: "right"})
+sr.reveal(".leftCategory", { origin: "left",delay:800})
+sr.reveal(".catProduct", { origin: "top" })
+sr.reveal(".swiper-slide", { origin: "bottom",delay:700 })
+sr.reveal(".leftPagsElm", { origin: "left",delay:700})
+sr.reveal(".clubAnimat")
+sr.reveal(".blogAnimate")
+sr.reveal(".contactAnimat", { origin: "top",delay:800})
+sr.reveal(".serviceAnimate", { origin: "right",delay:800})
+sr.reveal(".footerAnimate")
